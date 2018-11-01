1071633ca7abed5896e7cb47a6eaa0b403ebe910
RainersYT <35530071+RainersYT@users.noreply.github.com>
2018-11-01 17:49:26 +0200

    Create Obfuscator.lua

---
+++ Obfuscator.lua
@@ -0,0 +1,35 @@
+local Script = [[
+print("Hello world!")	
+]]
+Li1lI1Li1lIL1Il1lI1LILL1iLI1LilI1LI1Li1lI1Li1l = warn
+IL1Li1lI1Li1liL1Li1lI1Li1lI1Li1li1LI1iL1Ili1lI = "";
+li1l1I1l1i1L1IIl1iLI1L1ilI1Li1lI1Li1lI1Li11LI1 = li1l1I1l1i1L1IIl1iLI1L1ilI1Li1lI1Li1lI1Li11LI1;
+LI1li1L1ILi1lI1LI1liL1IL1iiLili1lI1L1iLI1LiLI1 = loadstring;
+LIi1LI1Lil1lI1IL1iLIlL1i1l1IL1iL1IIl1I1L1il1I1 = li1l1I1l1i1L1IIl1iLI1L1ilI1Li1lI1Li1lI1Li11LI1;
+getfenv()['ILiLIliLIliLIiliLiiiLI1li1L1I'] = LI1li1L1ILi1lI1LI1liL1IL1iiLili1lI1L1iLI1LiLI1;
+ILiLIliLIliLIiliLiiiLI1li1L1I([[print("Hello world!")]])()
+
+if not(LI1li1L1ILi1lI1LI1liL1IL1iiLili1lI1L1iLI1LiLI1) then
+	Li1lI1Li1lIL1Il1lI1LILL1iLI1LilI1LI1Li1lI1Li1l("")
+end
+
+function L1liL1L1i1liLI1Li1l1LILi1lI1L1iLI1Li1lI1Li1()
+	if(153/174/124/132/153/163/125/156/126/126/173/173/67/45/63/71/125/423/163/162) then
+		return not not not not not not not not not not not not not true
+	end
+end
+
+function li1lI1L11il11l1lILi1lI1Li1liLI1li1lI1LI1li1()
+	if(522/625/124/643/124/153/362/75/23/64/26/102/150/127/361/362/672/21/532/6431/212) then
+		return not not not not not not not not not not not not not false
+	end
+end
+
+-- Left off at : adding check against dumping
+
+
+-- ByteCode = 
+-- warn :
+-- loadstring : LI1li1L1ILi1lI1LI1liL1IL1iiLili1lI1L1iLI1LiLI1
+-- false : L1liL1L1i1liLI1Li1l1LILi1lI1L1iLI1Li1lI1Li1
+-- true : li1lI1L11il11l1lILi1lI1Li1liLI1li1lI1LI1li1
